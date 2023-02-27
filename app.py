7d9f141204a2bb86fb8773ef2652f8a4d18ead07
aashishkasar <109797716+aashishkasar@users.noreply.github.com>
2023-02-27 17:01:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title("Hello I am :blue[Aashish Kasar]")
+
+st.header('Welcome to My Portfolio! :sunglasses:' )
+
+st.subheader("I am Data Science Intern at :red[Innomatics Reserch Lab]")
+
+st.write('My Interest in Computer Vision')
+
+st.write('A punctual and hardworking student, very interested in learning new stuff and reserch further to unknown information. Looking forward to adapt and learn from training in the new environment, also meeting new friends and to work with together with them as a team.')
+
+st.subheader("Contact")
+
+st.write("Feel free to connect with me on any of the below platforms or send me an email at aashishkasar2011@gmail.com I'd love to hear from you!")
+
+st.markdown('[LinkdIn](https://www.linkedin.com/in/aashishkasar/)')
+
+st.markdown('[Github](https://github.com/aashishkasar)')
+
+st.markdown('[Email](aashishkasar2011@gmail.com)')
+
+st.write('Wellcome To AI World')
+st.image('https://www.simplilearn.com/ice9/free_resources_article_thumb/Data-Science-vs.-Big-Data-vs.jpg',caption='Wellcome to AI world')
